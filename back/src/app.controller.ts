1929451e95f9920d53db810c9f5b5b3de9e28ec5
nmacri9 <nicolasmacri09@gmail.com>
2026-06-02 19:57:02 -0300

    mi perfil

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
