9c575c3fcc5fe9c4659dac722089f7a36c157cdf
nmacri9 <nicolasmacri09@gmail.com>
2026-06-11 18:44:44 -0300

    estilo publis

--- front/src/app/pantallas/publicaciones/publicaciones.ts
+++ front/src/app/pantallas/publicaciones/publicaciones.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core'; 
 import { HttpClient } from '@angular/common/http';
+import { FormControl, ReactiveFormsModule } from '@angular/forms'; 
 import { Publicacion } from '../../componentes/publicacion/publicacion';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-publicaciones',
   standalone: true,
-  imports: [Publicacion],
+  imports: [Publicacion, ReactiveFormsModule], 
   templateUrl: './publicaciones.html',
   styleUrl: './publicaciones.css',
 })
 export class Publicaciones implements OnInit {
   public authService = inject(AuthService);
   private http = inject(HttpClient);
-  
   private cdr = inject(ChangeDetectorRef); 
   
   miUsuarioId = this.authService.usuarioActual()?._id;  
   listaPublicaciones: any[] = [];
+
+  // 👈 CONTROL PARA LA CAJA DE TEXTO
+  textoNuevaPublicacion = new FormControl('');
 
   ngOnInit() {
     this.cargarFeed();
@@ -38,6 +41,26 @@ export class Publicaciones implements OnInit {
         this.cdr.detectChanges(); 
       },
       error: (err) => console.error('Error al cargar el feed', err)
+    });
+  }
+
+  // 👈 FUNCIÓN PARA CREAR POSTEO
+  crearPublicacion() {
+    const texto = this.textoNuevaPublicacion.value;
+    if (!texto || !this.miUsuarioId) return;
+
+    // Armamos los datos tal cual los espera tu backend (descripcion y autor)
+    const nuevaPubli = {
+      descripcion: texto, 
+      autor: this.miUsuarioId 
+    };
+
+    this.http.post('https://nicolas-macri-tp-2-prog-4-2026-c1.vercel.app/publicaciones', nuevaPubli).subscribe({
+      next: () => {
+        this.textoNuevaPublicacion.setValue(''); 
+        this.cargarFeed(); 
+      },
+      error: (err) => console.error('Error al crear la publicación', err)
     });
   }
 
