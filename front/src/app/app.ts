009330ead1e826492bc30a9f6f317c5da3103b72
nmacri9 <nicolasmacri09@gmail.com>
2026-06-17 19:35:01 -0300

    diseño dashboard

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -65,7 +65,8 @@ export class App implements OnInit {
 
   iniciarCronometro() {
     clearTimeout(this.relojAviso);
-    const TIEMPO_ESPERA = 10 * 60 * 1000;
+    // const TIEMPO_ESPERA = 10 * 60 * 1000;
+    const TIEMPO_ESPERA = 5000;
 
     this.relojAviso = setTimeout(() => {
       this.mostrarModalRenovar = true;
