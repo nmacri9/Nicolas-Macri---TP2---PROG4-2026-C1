f0efb2b2711c0b88cf64cf08772ccfdbbd97122a
nmacri9 <nicolasmacri09@gmail.com>
2026-06-17 16:24:24 -0300

    dashboard-usuario

--- back/src/usuarios/usuarios.controller.ts
+++ back/src/usuarios/usuarios.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common'; // 👈 Agregamos 'UseGuards' acá
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { TokenGuard } from '../autenticacion/token/token.guard';
 
 @Controller('usuarios')
-@UseGuards(TokenGuard)
-export class UsuariosController {
+export class UsuariosController { 
   constructor(private readonly usuariosService: UsuariosService) {}
 
   @Post()
@@ -14,6 +13,7 @@ export class UsuariosController {
     return this.usuariosService.create(createUsuarioDto);
   }
 
+  @UseGuards(TokenGuard) 
   @Get()
   findAll() {
     return this.usuariosService.findAll();
@@ -29,11 +29,13 @@ export class UsuariosController {
     return this.usuariosService.update(+id, updateUsuarioDto);
   }
 
+  @UseGuards(TokenGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usuariosService.remove(id);
   }
 
+  @UseGuards(TokenGuard)
   @Post(':id/activar')
   activar(@Param('id') id: string) {
     return this.usuariosService.activar(id); 
